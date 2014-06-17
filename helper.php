fe870c06960cf3babf4118112fb1cf57a5052c27
Tomas Gavenciak <gavento@ucw.cz>
2014-06-17 16:05:23 +0200

    Add database initialisation to helper.php

--- helper.php
+++ helper.php
@@ -35,7 +35,44 @@ class helper_plugin_mamweb extends DokuWiki_Plugin {
 	    }
 	    $this->$dbh = $dbcon;
 	}
+
+	// Check current MaM DB version (if any)
+	$ver = $this->_getDbVersion();
+	msg("MaM-DB version found: $ver", 0);
+
+	// Update if newer version available
+	for ($v = $ver+1; $file = sprintf($updatedir.'/update%04d.sql', $i), file_exists($file); $v++) {
+	    $sql = io_readFile($file, false);
+
+	    $this->$dbh->beginTransaction();
+	    $res = $this->$dbh->exec($sql);
+	    if ($res === false) {
+		 msg("MaM DB update ($file) failed: ".$this->$dbh->rrorInfo(), -1);
+		 $this->$dbh->rollBack();
+		 return false;
+	    }
+	    $res = $this->$dbh->exec("INSERT INTO verze_db VALUES ($v, NOW());");
+	    if ($res === false) {
+		 msg("MaM DB version info update to ver $v failed: ".$this->$dbh->rrorInfo());
+		 $this->$dbh->rollBack();
+		 return false;
+	    }
+	    $this->$dbh->commit();	    
+	    msg("MaM DB updated to ver $v.", 2);
+	}
+
         return $this->$dbh;
+    }
+
+    /**
+     * Assumes $this->$dbh
+     * @return Version of the database schema
+     */
+    function _getDbVersion() {
+	$res = $this->$dbh->query('SELECT verze FROM verze_db LIMIT 1 ORDER BY verze DESC;')->fetch();
+	if ($res == false) 
+	    return 0;
+        return $res[0];
     }
 
 }
